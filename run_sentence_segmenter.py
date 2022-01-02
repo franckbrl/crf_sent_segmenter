1b55d26bc4c36b7f5f335671effc582c00ca1585
franckbrl <franck.burlot@gmail.com>
2022-01-02 20:46:39 +0100

    refacto

--- run_sentence_segmenter.py
+++ run_sentence_segmenter.py
@@ -8,14 +8,13 @@ import html
 from features import paragraph2features
 
 import mosestokenizer as mt
-import sklearn_crfsuite
 
 
 class preproc():
     def __init__(self, lang):
         self.normalize = mt.MosesPunctuationNormalizer(lang)
-        self.tokenize  = mt.MosesTokenizer(lang)
-        
+        self.tokenize = mt.MosesTokenizer(lang)
+
     def run_normalization(self, text):
         norm_text = self.normalize(text)
         norm_text = norm_text.replace("’", "'")
@@ -36,10 +35,10 @@ class preproc():
         any character (other than space).
         """
         len_before = len(before.replace(" ", ""))
-        len_after  = len( after.replace(" ", ""))
+        len_after = len(after.replace(" ", ""))
         assert len_before == len_after
 
-        
+
 def preprocess_input(text, pproc):
     """
     Get normalized and tokenized paragraphs
@@ -51,7 +50,8 @@ def preprocess_input(text, pproc):
         pp_par = pproc.run_normalization(parag)
         pp_par = pproc.run_tokenization(parag)
         yield parag, pp_par
- 
+
+
 def split_sentences(parag, pp_par, labels):
     """
     Split initial raw sentences according to predicted labels.
@@ -68,18 +68,26 @@ def split_sentences(parag, pp_par, labels):
             sent = ""
     if sent:
         yield sent
-            
+
+
 def arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', dest='inp', nargs='?', type=argparse.FileType('r'),
+    parser.add_argument('-i', dest='inp', nargs='?',
+                        type=argparse.FileType('r'),
                         default=sys.stdin, help="input data")
-    parser.add_argument('-o', dest='out', nargs='?', type=argparse.FileType('w'),
+    parser.add_argument('-o', dest='out', nargs='?',
+                        type=argparse.FileType('w'),
                         default=sys.stdout, help="output data")
-    parser.add_argument('--model', dest='model', nargs='?', type=str,
-                        default="sseg_model.pkl", help="Name of model file (default: %(default)s).")
-    parser.add_argument('--lang', dest='lang', nargs='?', type=str,
-                        default="fr", help="Preprocessing language (default: %(default)s).")
+    parser.add_argument('--model', dest='model',
+                        nargs='?', type=str,
+                        default="sseg_model.pkl",
+                        help="Name of model file (default: %(default)s).")
+    parser.add_argument('--lang', dest='lang',
+                        nargs='?', type=str,
+                        default="fr",
+                        help="Preprocessing language (default: %(default)s).")
     return parser.parse_args()
+
 
 def main(args):
     # pre-processing steps
@@ -91,16 +99,9 @@ def main(args):
     for parag, pp_par in preprocess_input(args.inp, pproc):
         feat = [paragraph2features(pp_par)]
         labels = crf.predict(feat)[0]
-        sents  = split_sentences(parag, pp_par, labels)
+        sents = split_sentences(parag, pp_par, labels)
         for sent in sents:
             args.out.write(sent + "\n")
 
 
 main(arguments())
-
-
-
-
-
-
-
